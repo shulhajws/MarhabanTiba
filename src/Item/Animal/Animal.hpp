f6ec8570bf18f82aee3b1960c423a6d9a48031b6
shulhajws <13522087@std.stei.itb.ac.id>
2024-04-05 12:13:24 +0700

    fix: Animal (minor)

--- src/Item/Animal/Animal.hpp
+++ src/Item/Animal/Animal.hpp
@@ -16,7 +16,7 @@ class Animal : public Item{
         Animal(string code, string name, string type, int harvestWeight, int price);
         Animal(string code, string name, string type, int animalWeight,int harvestWeight,int id);
         void setAnimalWeight(int animalWeight); // buat muat berat hewan
-        virtual void makan(Products p)=0;
+        virtual void makan(Products* p)=0;
         bool isHarvestable();
 };
 
