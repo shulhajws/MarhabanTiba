b0389b8a130f2fc626423378eebe0b86b6ba3051
nabilashikoofa <79271432+nabilashikoofa@users.noreply.github.com>
2024-04-16 14:22:50 +0700

    handle buy edge case

--- src/Command/AllCommand/Beli.cpp
+++ src/Command/AllCommand/Beli.cpp
@@ -1,10 +1,16 @@
 #include "Beli.hpp"
 
 void Beli::useCommand(){
-    players[currPlayer]->buyItem();
+    if (players[currPlayer]->getPlayerWealth() > 2){
+        players[currPlayer]->buyItem();
+    } else {
+        cout << "Your current money is " << players[currPlayer]->getPlayerWealth() << " gulden.\033[1;31m\nGo make that money, then you can splurge on whatever you want.\033[0m" << endl;
+    }
 }
 
 string Beli::getName()
 {
     return "BELI";
 }
+
+
